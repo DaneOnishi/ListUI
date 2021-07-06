b07c3ef85e2ad235a61fd480f2cbb28bc8c30847
DaneOnishi <daniellaonishi07@gmail.com>
2021-07-06 01:34:02 -0300

    Initial Commit

---
+++ ListUI/ListUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListUIApp.swift
+//  ListUI
+//
+//  Created by Daniella Onishi on 06/07/21.
+//
+
+import SwiftUI
+
+@main
+struct ListUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
